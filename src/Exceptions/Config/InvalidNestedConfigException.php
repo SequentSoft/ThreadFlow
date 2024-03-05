fa82c6634f5d365f0f0ea97bdcea622df3cd8a7f
Sergiy Litvinchuk <sergiy.litvinchuk@gmail.com>
2024-03-05 15:48:49 +0200

    fix cs

--- src/Exceptions/Config/InvalidNestedConfigException.php
+++ src/Exceptions/Config/InvalidNestedConfigException.php
@@ -7,8 +7,15 @@ use SequentSoft\ThreadFlow\Contracts\Config\ConfigInterface;
 
 class InvalidNestedConfigException extends Exception
 {
-    public function __construct(ConfigInterface $config, string $key)
+    public function __construct(protected ConfigInterface $config, string $key)
     {
-        parent::__construct("The nested config key [{$key}] does not exist in the config.");
+        parent::__construct(
+            "The nested config key [{$key}] does not exist in the config."
+        );
+    }
+
+    public function getConfig(): ConfigInterface
+    {
+        return $this->config;
     }
 }
